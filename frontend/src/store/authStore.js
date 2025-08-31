268a6d6e605d16dd1d2ea2ab36d39c4fcf60b68e
Harshil Kachhadiya <harshil23kachhadiya@gmail.com>
2025-09-01 00:00:43 +0530

    changes URLs

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth" ;
+const API_URL = "http://outh.netlify.app/api/auth" ;
 
 axios.defaults.withCredentials = true;
 
